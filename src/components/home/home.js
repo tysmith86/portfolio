ebf108eadfbcea9baa8f5af80a98997f4b844635
Tyler Smith <tyler.smith6891@gmail.com>
2017-04-25 16:54:53 -0700

    completed home screen

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -9,4 +9,6 @@ export default {
 
 function controller() {
   this.styles = styles;
+
+  
 }
--- src/components/skills/skills.js
+++ src/components/skills/skills.js
@@ -1,10 +1,12 @@
 import template from './skills.html';
+import styles from './skills.scss';
 
 export default {
   template,
+  styles,
   controller
 };
 
 function controller(){
-  
+  this.styles = styles;
 }
