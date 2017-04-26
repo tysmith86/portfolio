fa7704a344876dabb5bfd0d6014f90e89945ca1a
Tyler Smith <tyler.smith6891@gmail.com>
2017-04-26 16:04:08 -0700

    commented out ui-router in order to use nav page-location  jumps

--- src/main.js
+++ src/main.js
@@ -2,21 +2,21 @@ import angular from 'angular';
 import components from './components';
 // import services from './services';
 import './scss/main.scss';
-import uiRouter from 'angular-ui-router';
+// import uiRouter from 'angular-ui-router';
 // import defaultRoute from 'angular-ui-router-default';
 // import http from './http';
-import routes from './routes';
+// import routes from './routes';
 // import auth from './auth';
 
 const app = angular.module('portfolio', [
   components,
   // services,
-  uiRouter
+  // uiRouter
   // defaultRoute
 ]);
 
 // app.config(http);
-app.config(routes);
+// app.config(routes);
 // app.run(auth);
 
 const dev = process.env.API_URL || '/api';
