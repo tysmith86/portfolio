e48452adb7aafe6709e0108ea6bd1fc8dbae5972
Tyler Smith <tyler.smith6891@gmail.com>
2017-05-01 18:23:02 -0700

    add social icons and various styling

---
+++ src/components/contact/contact.js
@@ -0,0 +1,12 @@
+import template from './contact.html';
+import styles from './contact.scss';
+
+export default {
+  template,
+  styles,
+  controller
+};
+
+function controller() {
+  this.styles = styles;
+};
